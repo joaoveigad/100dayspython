b51379218d9bf880499b2952146058e706b9aa0f
João Veiga <jvdvandrade1996@gmail.com>
2025-12-15 23:25:24 -0300

    day 1: printing

---
+++ day 1/bandnamesim.py
@@ -0,0 +1,4 @@
+print('Welcome to your Band Name Generator.')
+cityName = input("What's the name of the city you grew up in? \n")
+petName = input("Cool! So, what's your pet's name? \n")
+print(f'Your band name is {cityName} {petName}!')
