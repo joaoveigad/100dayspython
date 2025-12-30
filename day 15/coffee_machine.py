234229fe8b9e1bae24bd012d965d7ad1b0664f9b
João Veiga <jvdvandrade1996@gmail.com>
2025-12-29 23:54:19 -0300

    money resource logic + change

---
+++ day 15/coffee_machine.py
@@ -0,0 +1,86 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18
+        },
+        "cost": 1.5
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "cost": 2.5
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24
+        },
+        "cost": 3.0
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0.0
+}
+
+def compare_resources(user_choice):
+    for resource, amount in MENU[user_choice]["ingredients"].items():
+        if resources[resource] < amount:
+            print(f'Sorry, there is not enough {resource}')
+            return False
+    print("yes you did it... you did order your coffee... oh yes... coffee ordered. ok good yes.")
+    return True
+
+def transaction(user_choice):
+    pennies = int(input("How many one (1) cent pennies would you like to insert? "))
+    nickles = int(input("How many five (5) cent nickles would you like to insert? "))
+    dimes = int(input("How many ten (10) cent dimes would you like to insert? "))
+    quarters = int(input("How many twenty-five (25) cent quarters would you like to insert? "))
+
+    value_inserted = (
+        pennies * 0.01 +
+        nickles * 0.05 +
+        dimes * 0.10 +
+        quarters * 0.25
+    )
+
+    if value_inserted < MENU[user_choice]["cost"]:
+        print("Not enough cash! Stranger...")
+        return False
+    elif value_inserted > MENU[user_choice]["cost"]:
+        change = round(value_inserted - MENU[user_choice]["cost"], 2)
+        print(f'Your change: {change}')
+        resources["money"] += MENU[user_choice]["cost"]
+        return True
+    return True
+
+
+def turn_on():
+    while True:
+        user_choice = input("What would you like? (espresso / latte / cappuccino): ").lower()
+        match user_choice:
+            case "report":
+                print(resources)
+            case "espresso" | "latte" | "cappuccino":
+                print(f'You chose: {user_choice}')
+                compare_resources(user_choice)
+                transaction(user_choice)
+                print("Coffee ordered!")
+            case "off":
+                return
+            case _:
+                print("Invalid option")
+
+
+
+turn_on()
+
